1ecba30f754fb350acf92d5a91223589220a4fe1
johe <johe@hell.com>
2024-05-27 13:37:38 +0200

    Fixa så att man inte kan logga ut och sedan gå tillbaka till Chat

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -2,15 +2,20 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
+import Navbar from './Navbar';
 
 const PrivateRoute = ({ element }) => {
-  const { isAuthenticated, loading } = useAuth();
-
-  if (loading) {
-    return <div>Loading...</div>;
+  const { isAuthenticated } = useAuth();
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
   }
 
-  return isAuthenticated ? element : <Navigate to="/login" />;
+  return (
+    <>
+      <Navbar />
+      {element}
+    </>
+  );
 };
 
 export default PrivateRoute;
